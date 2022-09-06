caa2996129c40a9e3fe2f6ce332ba4e354da0754
Bastien Javaux <bastienjavaux@gmail.com>
2022-09-06 09:15:11 +0200

    2day commit

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+// Swal.fire({
+//   title: "Error!",
+//   text: "Do you want to continue",
+//   icon: "success",
+//   confirmButtonText: "Cool",
+// });
+
+let addtaskButton = document
+  .getElementById("addTaskAction")
+  .addEventListener("click", addTask);
+let task = document.getElementById("task");
+let app = document.getElementById("todoApp");
+
+function addTask() {
+  let createTask = document.createElement("div");
+}
